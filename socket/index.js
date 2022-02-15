eb2edc0e39b1b91069893342ff14fc3624dae5ec
huubinh49 <hocchui0409@gmail.com>
2022-02-15 17:42:04 +0700

    add confirm call

--- socket/index.js
+++ socket/index.js
@@ -1,47 +1,47 @@
 const socket = require("socket.io");
 
-class IDManager{
-  constructor(){
+class IDManager {
+  constructor() {
     this.ids = {};
   }
-  add(userId, id){
+  add(userId, id) {
     this.ids[userId] = id;
   };
-  
-  remove(id){
-    for(let user in this.ids)
-    if(this.ids[user] == id){
-      this.ids[user]= null;
-      delete this.ids[user]
-    }
+
+  remove(id) {
+    for (let user in this.ids)
+      if (this.ids[user] == id) {
+        this.ids[user] = null;
+        delete this.ids[user]
+      }
   };
-  
-  get(userId){
+
+  get(userId) {
     return this.ids[userId];
   };
-  get_reversed(id){
-    for(let user in this.ids)
-    if(this.ids[user] == id){
-     return user;
-    }
+  get_reversed(id) {
+    for (let user in this.ids)
+      if (this.ids[user] == id) {
+        return user;
+      }
     return null;
   }
 }
-class Socket{
-  constructor(){
+class Socket {
+  constructor() {
     console.log("Initializing socket")
     this.io = null;
     this.UserSocket = new IDManager(); // userId - socketId
     this.users = {};
     this.socketRoomMap = {};
   }
- 
-  getPartnerPeerJS(peerid){
-    for(let peer of this.peers){
-      if(peerId == peer[0]){
+
+  getPartnerPeerJS(peerid) {
+    for (let peer of this.peers) {
+      if (peerId == peer[0]) {
         return peer[1]
       }
-      if(peerId == peer[1]){
+      if (peerId == peer[1]) {
         return peer[0]
       }
     }
@@ -49,47 +49,56 @@ class Socket{
   }
   init(httpServer) {
 
-    this.io = socket(httpServer,  {
+    this.io = socket(httpServer, {
       cors: {
         origins: ['http://localhost:3000']
       }
     });
     console.log('Socket is ready!')
-    
+
     this.io.on("connection", (socket) => {
       //when connect
       console.log("A user connected.");
-      
+
       //take userId and socketId from user
       socket.on("addUser", (payload) => {
-        console.log("add user: ", payload)
+        console.log("add user: ", this.UserSocket.ids)
         this.UserSocket.add(payload.userId, socket.id);
       });
-      socket.on("call-request", (payload) => {
-        console.log("Calling to")
-        this.io.to(this.UserSocket.get(payload.toUID)).emit("call-request", payload);
+      const events = ['call-request', 'accepted', 'rejected', 'cancel-call']
+      events.forEach(event => {
+        socket.on(event, (payload) => {
+          this.io.to(this.UserSocket.get(payload.toUID)).emit(event, payload);
+        })
       })
+
       socket.on('join-room', (roomId, userDetails) => {
         // adding all user to a room so that we can broadcast messages
         socket.join(roomId);
-    
+
         // adding map users to room
         if (this.users[roomId]) {
-          this.users[roomId].push({ socketId: socket.id, ...userDetails });
+          this.users[roomId].push({
+            socketId: socket.id,
+            ...userDetails
+          });
         } else {
-          this.users[roomId] = [{ socketId: socket.id, ...userDetails }];
+          this.users[roomId] = [{
+            socketId: socket.id,
+            ...userDetails
+          }];
         }
-    
+
         // adding map of socketid to room
         this.socketRoomMap[socket.id] = roomId;
         const usersInThisRoom = this.users[roomId].filter(
           (user) => user.socketId !== socket.id
         );
-    
+
         // once a new user has joined sending the details of users who are already present in room.
         socket.emit('users-present-in-room', usersInThisRoom);
       });
-    
+
       socket.on('initiate-signal', (payload) => {
         const roomId = this.socketRoomMap[socket.id];
         let room = this.users[roomId];
@@ -98,7 +107,7 @@ class Socket{
           const user = room.find((user) => user.socketId === socket.id);
           fullName = user.fullName;
         }
-    
+
         // once a peer wants to initiate signal, To old user sending the user details along with signal
         this.io.to(payload.userToSignal).emit('user-joined', {
           signal: payload.signal,
@@ -106,7 +115,7 @@ class Socket{
           fullName,
         });
       });
-    
+
       // once the peer acknowledge signal sending the acknowledgement back so that it can stream peer to peer.
       socket.on('ack-signal', (payload) => {
         this.io.to(payload.callerId).emit('signal-accepted', {
@@ -119,20 +128,20 @@ class Socket{
         console.log("new message: ", message)
         const recipientSocket = this.UserSocket.get(message.recipientId);
         const senderSocket = this.UserSocket.get(message.senderId);
-        try{
-          if(recipientSocket)
-          this.io.to(recipientSocket).emit("newMessage", message);
-          if(senderSocket)
-          this.io.to(senderSocket).emit("newMessage", message);
-        }catch(error){
+        try {
+          if (recipientSocket)
+            this.io.to(recipientSocket).emit("newMessage", message);
+          if (senderSocket)
+            this.io.to(senderSocket).emit("newMessage", message);
+        } catch (error) {
           console.log(error)
         }
       });
       //when disconnect
       socket.on("disconnect", () => {
-        
+
         const roomId = this.socketRoomMap[socket.id];
-        if(roomId){
+        if (roomId) {
           console.log("a call disconnected!");
           // Remove this user from room 
           let room = this.users[roomId];
@@ -140,9 +149,9 @@ class Socket{
             room = room.filter((user) => user.socketId !== socket.id);
             this.users[roomId] = room;
           }
-          // on disconnect sending to all users that user has disconnected
-          socket.to(roomId).broadcast.emit('user-disconnected', socket.id);
-        }else{
+          // TODO: on disconnect sending to all users that user has disconnected
+          socket.to(roomId).emit('user-disconnected', socket.id);
+        } else {
           console.log("a user disconnected!");
           this.UserSocket.remove(socket.id);
         }
@@ -150,7 +159,7 @@ class Socket{
     });
     return this.io;
   }
-  getIO (){
+  getIO() {
     if (!this.io) {
       throw new Error('Socket.io is not initialized')
     }
@@ -159,15 +168,15 @@ class Socket{
   sendTo(userId, event, payload) {
     try {
       let userSocket = this.UserSocket.get(userId);
-      if(userSocket){
+      if (userSocket) {
         console.log("Sending event ", event, "payload: ", payload)
-        this.io.to(userSocket).emit(event, payload) 
+        this.io.to(userSocket).emit(event, payload)
       }
     } catch (error) {
       console.log(error)
     }
   }
-  
+
 }
 const socketServer = new Socket()
 module.exports = socketServer
